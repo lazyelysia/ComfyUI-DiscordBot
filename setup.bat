7544283b59c860fd7ac7e6f9f774a3321ed53dd3
lazyelysia <lazyelysia@gmail.com>
2026-01-06 22:52:01 +0000

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,3 +1,10 @@
+@echo off
+
+echo Setting up virtual environment...
 python -m venv ./.venv
+
+echo Installing requirements...
 "./.venv/Scripts/python.exe" -m pip install -r requirements.txt
+
+echo Installation complete!
 pause
