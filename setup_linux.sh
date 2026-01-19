c8452e2fc4f7c99ce1e004fca6ba3431b1182697
lazyelysia <lazyelysia@gmail.com>
2026-01-19 11:20:09 +0000

    Enhance setup script for virtual environment
    Added virtual environment setup and installation steps.

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+
+echo "Setting up virtual environment..."
 python3 -m venv .venv
+
+echo "Installing requirements..."
 ./.venv/bin/python -m pip install -r requirements.txt
+
+echo "DiscordBot setup complete! Configure 'config.properties' and restart to run the bot."
